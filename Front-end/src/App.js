c4563fa7e2d969c30cc00d5b6d12a22e7d4d0ad8
Safwan Abbas <safwanabbas2020@gmail.com>
2023-02-09 15:57:35 +0500

    Authentication of InActive Account

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
+import Activate from "./pages/home/activate";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
         <Route element={<LoggedInRoutes />} />
         <Route path="/profile" element={<Profile />} exact />
         <Route path="/" element={<Home />} exact />
+        <Route path="/activate/:id" element={<Activate />} exact />
       </Routes>
     </div>
   );
