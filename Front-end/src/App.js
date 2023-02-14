c250bf457d96d8d35c44d6e84f6c5c77cdca5029
Safwan Abbas <safwanabbas2020@gmail.com>
2023-02-14 17:02:07 +0500

    post screen

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,12 +3,16 @@ import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
-import Activate from "./pages/home/activate";
 import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+import { useSelector } from "react-redux";
+import Activate from "./pages/home/activate";
 import Reset from "./pages/reset";
+import CreatePostPopup from "./components/createPostPopup";
 function App() {
+  const { user } = useSelector((state) => ({ ...state }));
   return (
     <div>
+      <CreatePostPopup user={user} />
       <Routes>
         <Route element={<LoggedInRoutes />}>
           <Route path="/profile" element={<Profile />} exact />
---
+++ Front-end/src/components/createPostPopup/index.js
@@ -0,0 +1,28 @@
+import "./style.css";
+export default function CreatePostPopup({ user }) {
+  return (
+    <div className="blur">
+      <div className="postBox">
+        <div className="box_header">
+          <div className="small_circle">
+            <i className="exit_icon"></i>
+          </div>
+          <span>Create Post</span>
+        </div>
+        <div className="box_profile">
+          <img src={user.picture} alt="" />
+          <div className="box_col">
+            <div className="box_profile_name">
+              {user.first_name} {user.last_name}
+            </div>
+            <div className="box_privacy">
+              <img src="../../../icons/public.png" alt="" />
+              <span>Public</span>
+              <i className="arrowDown_icon"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
